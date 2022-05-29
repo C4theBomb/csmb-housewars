4a5d2b8a22b37fc0375b0fac2e16a5d0cc244b93
C4 Patino <c4patino@gmail.com>
2022-05-29 12:01:49 -0500

    Decided to nix test suite

--- housewars/tests.py
+++ housewars/tests.py
@@ -1,18 +1,1 @@
 from django.test import TestCase
-
-
-class SignupViewTests(TestCase):
-    def test_working_input(self):
-        pass
-
-    def test_duplicate_email(self):
-        pass
-
-    def test_more_than_60_minutes(self):
-        pass
-
-    def test_no_full_activities(self):
-        pass
-
-    def test_no_60_in_slot_2(self):
-        pass
