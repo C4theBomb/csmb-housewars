13e8a02c7cc73fd3e6577c04bee48bbfff65d27c
C4 Patino <c4patino@gmail.com>
2022-08-29 21:54:57 -0500

    Added whitenoise for staticfiles

--- CollegiateHouseWars/settings/prod.py
+++ CollegiateHouseWars/settings/prod.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'CollegiateHouseWars.urls'
@@ -123,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
