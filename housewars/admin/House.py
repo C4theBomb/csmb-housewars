36fa4b13fc1b280a7e27398b63d6d0a8c69b6d4f
C4 Patino <c4patino@gmail.com>
2022-09-03 11:56:21 -0500

    Added ability to see current house points

--- housewars/admin/House.py
+++ housewars/admin/House.py
@@ -5,13 +5,13 @@ from ..models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'points')
+    list_display = ('name', 'points', 'current_points', 'total_points')
     actions = ['finalize_points']
 
-    # @admin.display(description='Current Housewars Points')
-    # def current_points(self, obj):
-    #     return obj.current_points
+    @admin.display(description='Current Housewars Points')
+    def current_points(self, obj):
+        return obj.current_points
 
-    # @admin.display(description='Total Points')
-    # def total_points(self, obj):
-    #     return obj.total_points
+    @admin.display(description='Total Points')
+    def total_points(self, obj):
+        return obj.total_points
