b9cf6d0753db056d38d27d53155d4e4d20e716d7
C4 Patino <c4patino@gmail.com>
2022-09-07 15:14:42 -0500

    Made unpack_values work with custom headers and exclusions

--- housewars/utils/unpack_values.py
+++ housewars/utils/unpack_values.py
@@ -1,20 +1,21 @@
+import string
 from django.db.models import QuerySet
 from typing import List
 
 
-def unpack_values(queryset: QuerySet) -> List[List]:
+def unpack_values(queryset: QuerySet, headers: List[str]) -> List[List]:
     """Unpacks a queryset into a 2D array
 
     :param queryset: The queryset that will be unpacked
+    :param headers: The headers of the 
     :returns A table headers, then rows
     """
 
-    field_names = [field.name for field in queryset.model._meta.fields]
     data = []
-    data.append(field_names)
+    data.append(headers)
     for row in queryset:
         values = []
-        for field in field_names:
+        for field in headers:
             value = getattr(row, field)
             if value is None:
                 value = ''
